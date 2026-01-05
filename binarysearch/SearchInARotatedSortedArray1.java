8a94d84475e96b02652db763b9c1f54dcb1f4cc2
Piyush Prajapati <piyushprajapati7120@gmail.com>
2026-01-05 20:06:19 +0530

    Solved Two Leetcode Prblms

---
+++ binarysearch/SearchInARotatedSortedArray1.java
@@ -0,0 +1,31 @@
+package binarysearch;
+
+public class SearchInARotatedSortedArray1 {
+    public int search(int[] arr, int target){
+        
+        int low = 0;
+        int high = arr.length - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+
+            if(arr[low] <= arr[mid]){
+                if(arr[low] <= target && target <= arr[mid]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }else{
+                if(arr[mid] <= target && target <= arr[high]){
+                    high = mid + 1;
+                }else{
+                    low = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ binarysearch/SearchInARotatedSortedArrray2.java
@@ -0,0 +1,36 @@
+package binarysearch;
+
+public class SearchInARotatedSortedArrray2 {
+    public boolean search(int[] arr, int target){
+        int low = 0;
+        int high = arr.length - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] == target){
+                return true;
+            }
+
+            if(arr[low] == arr[mid] && arr[mid] == arr[high]){
+                low++;
+                high--;
+                continue;
+            }
+
+            if(arr[low] <= arr[mid]){
+                if(arr[low] <= target && target <= arr[mid]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }else{
+                if(arr[mid] <= target && target <= arr[high]){
+                    high = mid + 1;
+                }else{
+                    low = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
