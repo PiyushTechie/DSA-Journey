f611550e2ec2ae2b87892b7e89e1c91e68fab5cd
Piyush Prajapati <piyushprajapati7120@gmail.com>
2026-01-08 22:48:16 +0530

    Two More Questions

---
+++ binarysearch/squareRootInLogn.java
@@ -0,0 +1,18 @@
+package binarysearch;
+
+public class squareRootInLogn {
+    public static int findSqrt(int n){
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid = low + (high - low)/2;
+            if(mid * mid <= n){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+
+        return (int)Math.floor(high);
+    }
+}
